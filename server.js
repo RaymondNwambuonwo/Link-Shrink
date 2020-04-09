3cf65aec0a6d58238ae221be66c82db97ded5f8b
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2020-04-08 21:16:13 -0400

    mongoose connection established

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect("mongodb://localhost/linkShrink", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// app.post("/shortLinks", (req, res) => {});
 
 app.listen(process.env.PORT || 4000);
